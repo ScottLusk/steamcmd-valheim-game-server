ee20f8585620e323cf22c099aee5734428fdad27
Scott Lusk <Scott.t.lusk@gmail.com>
2021-02-21 14:58:41 -0600

    fix storage

--- src/start_server.sh
+++ src/start_server.sh
@@ -2,4 +2,4 @@
 
 cd ${STEAMAPPDIR}
 
-./valheim_server.x86_64 -nographics -batchmode -savedir "~/valheimserver/Storage" -name "$WORLD_NAME" -port 2456 -world "$WORLD_NAME" -password "$PASSWORD"
+./valheim_server.x86_64 -nographics -batchmode -savedir "Storage" -name "$WORLD_NAME" -port 2456 -world "$WORLD_NAME" -password "$PASSWORD"
